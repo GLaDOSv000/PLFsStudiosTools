ddc0dc5da6b2c68b7d16fc2d069108d088d9b12a
GlaDOSv000 <migueljoserodriguezlorenzo09@gmail.com>
2026-08-24 18:28:57 -0400

    v0.2

--- imageSplitter.py
+++ imageSplitter.py
@@ -10,13 +10,15 @@ img_name = ""
 def clearScreen():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("-PLF's-Studios---Separador-de-texturas-de-UE------v0.1-------")
+def espacios():
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
 
 def SolicitarArchivo():
-    print("")
-    print("")
-    print("")
-    print("")
-    print("")
+    
     temp = input("Suelte el archivo aqui: ")
     if temp[-1:] == " ":
         temp = temp[:len(temp) - 1]
@@ -29,31 +31,26 @@ def SolicitarArchivo():
     return(temp)
 
 def procesarimg(stringruta):
+    decidir = input("roughness color? y/n [n]")
     img = Image.open(stringruta).convert("RGB")
     r, g, b = img.split()
     cero = Image.new("L", img.size, 0)
+
+    #OCLUSSION
     img_new_final = Image.merge("RGB", (r, r, r))
-    #img_new_final = img_new.convert("L")
-    #arr = np.array(img_new, dtype=np.float32) / 255.0
-    #gris_lineal = np.mean(arr, axis=2)
-    #resultado = (gris_lineal * 255).astype(np.uint8)
-    #img_new_final = Image.fromarray(resultado)
     print("output/" + os.path.basename(stringruta) + "_oclussion.png")
     img_new_final.save("output/" + os.path.basename(stringruta) + "_oclussion.png")
 
+    #METAlNESS
     img_new_final = Image.merge("RGB", (b, b, b))
-    #arr = np.array(img_new, dtype=np.float32) / 255.0
-    #gris_lineal = np.mean(arr, axis=2)
-    #resultado = (gris_lineal * 255).astype(np.uint8)
-    #img_new_final = Image.fromarray(resultado)
     print("output/" + os.path.basename(stringruta) + "_metalness.png")
     img_new_final.save("output/" + os.path.basename(stringruta) + "_metalness.png")
 
-    img_new_final = Image.merge("RGB", (g, g, g))
-    #arr = np.array(img_new, dtype=np.float32) / 255.0
-    #gris_lineal = np.mean(arr, axis=2)
-   # resultado = (gris_lineal * 255).astype(np.uint8)
-  #  img_new_final = Image.fromarray(resultado)
+    #ROUGHNESS
+    if decidir == "y"or "Y":
+        img_new_final = Image.merge("RGB", (cero, g, cero))
+    else:
+        img_new_final = Image.merge("RGB", (g, g, g))
     print("output/" + os.path.basename(stringruta) + "_roughness.png")
     img_new_final.save("output/" + os.path.basename(stringruta) + "_roughness.png")
 
